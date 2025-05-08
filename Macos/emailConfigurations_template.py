69e3826a95c76a99d74182d5d5c418caf6fcaf39
unknown <arvydaslozysak@gmail.com>
2025-05-08 17:05:08 +0300

    changed files

---
+++ Macos/emailConfigurations_template.py
@@ -0,0 +1,11 @@
+EMAIL_ADDRESS = 'printer_email@example.com'
+EMAIL_PASSWORD = 'printer_email_password'
+SMTP_SERVER = 'smtp.example.com'
+IMAP_SERVER = 'imap.example.com'
+TO_EMAIL = 'recipient_email@example.com'
+
+'''
+EMAIL_ADDRESS is the email configured for the 3d printer. this is the email that sends the detected failure
+
+TO_EMAIL is the recipient email, this is the email which will receive the message about a print failure detection
+'''
