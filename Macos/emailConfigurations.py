2dbc6be0840785ef747f9ae6ee328e292aea0702
unknown <arvydaslozysak@gmail.com>
2025-05-08 17:01:48 +0300

    removed files

--- Macos/emailConfigurations.py
+++
@@ -1,6 +0,0 @@
-EMAIL_ADDRESS = 'dprinter68@gmail.com'
-EMAIL_PASSWORD = 'gxnm jzbk tivm eqyg'
-SMTP_SERVER = 'smtp.gmail.com'
-IMAP_SERVER = 'imap.gmail.com'
-TO_EMAIL = 'arvydaslozysak@gmail.com'
-
