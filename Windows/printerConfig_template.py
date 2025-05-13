8cfe20a7ddc991498e92126a468d3b08a4851e8b
unknown <arvydaslozysak@gmail.com>
2025-05-13 16:26:20 +0300

    updated printerconfig file

--- Windows/printerConfig_template.py
+++ Windows/printerConfig_template.py
@@ -1,3 +1,12 @@
-PRINTER_IP = 'xxx.xx.xx.x'
-
-#Ip address of the 3d printer
+printer_configs = [
+    {
+        "printer_name": "Printer 1",
+        "printer_ip": "xxx.xxx.x.xxx",
+        "camera_url": "http://192.168.x.xxx:8080/?action=stream"
+    },
+    {
+        "printer_name": "Printer 2",
+        "printer_ip": "xxx.xxx.x.xxx",
+        "camera_url": "http://192.168.x.xxx:8080/?action=stream"
+    }
+]
